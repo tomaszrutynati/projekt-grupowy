8377e3521b46d8e55c48ccec685abef9bfeec729
Tomasz.Rutyna <Poniatowa2021!>
2020-09-26 12:32:07 +0200

    init

---
+++ src/main/java/pl/sda/demoprojectgroup/DemoProjectGroupApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.demoprojectgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectGroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProjectGroupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/demoprojectgroup/DemoProjectGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.demoprojectgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectGroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
